1889948a1e3d75a9db1e963328fcc981c57a8c9d
RonRed3 <ron754895@gmail.com>
2022-03-08 18:31:09 +0200

    UPDATE

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
+#!/usr/bin/env pybricks-micropython
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.ev3devices import InfraredSensor, UltrasonicSensor, GyroSensor
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
-import time, PID, motor
+import func.PID as PID
+#import run.3
+#import _MOTOR-CONTROLL_
+#motor.time_move("D",100, 100)
+#import mc
+#while True:
+    #motor.time_move("C", 0.1, 100)
+    #motor.time_move("B", 0.1, 100)
+    
+#PID.follow_line(80,15)
+#import CONF.MotorControll
+import run.one
+#PID.turn(45)
+#PID.turn(45)
 
-PID.drive(55,70)
-time.sleep(0.0001)
-PID.drive(35,10)
-PID.drive(-12,40)
+#print(1) 
+
+
+
+
+
--- run/one.py
+++ run/one.py
@@ -1,28 +1,17 @@
-#!/usr/bin/env pybricks-micropython
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.ev3devices import InfraredSensor, UltrasonicSensor, GyroSensor
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import time
 import func.PID as PID
-#import run.3
-#import _MOTOR-CONTROLL_
-#motor.time_move("D",100, 100)
-#import mc
-#while True:
-    #motor.time_move("C", 0.1, 100)
-    #motor.time_move("B", 0.1, 100)
-    
-#PID.follow_line(80,15)
-#import CONF.MotorControll
-import run.three
-#PID.turn(45)
-#PID.turn(45)
-
-#print(1) 
+import func.motor as motor
 
 
-
-
-
+motor.time_move("A", 2, -20)
+PID.drive(100, 70)
+PID.drive(50, 50)
+PID.drive(70, 80)
+motor.time_move("D", 5, 120)
+PID.drive(-70, 200)
