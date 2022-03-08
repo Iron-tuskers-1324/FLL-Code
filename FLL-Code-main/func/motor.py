57d7bd2e719808a814421fd12ffda0980f6fd6dd
RonRed3 <ron754895@gmail.com>
2022-03-08 11:09:10 +0200

    UPDATE

--- FLL-Code-main/func/motor.py
+++ FLL-Code-main/func/motor.py
@@ -5,7 +5,16 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import math
 import time
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+middle_motorA = Motor(Port.A)
+robot = DriveBase(left_motor, right_motor, wheel_diameter = 290, axle_track = 120)
+robot.settings(straight_speed = 200, straight_acceleration = 100, turn_rate = 100)
+gyro = GyroSensor(Port.S2)
+
+
 
 def time_move(m, Time, speed):
     speed = speed * 10
@@ -23,3 +32,44 @@ def angle_move(m, angle, speed):
     motor = Motor(D[m])
     motor.run_angle(speed, angle)
     motor.hold()
+
+
+
+def on_line():
+    Black = 6
+    White = 50
+    speed = -40
+    left_motor = Motor(Port.B)
+    right_motor = Motor(Port.C)
+    robot = DriveBase(left_motor, right_motor, wheel_diameter = 290, axle_track = 120)
+    Lsensor = ColorSensor(Port.S1)
+    Rsensor = ColorSensor(Port.S4)
+    while Rsensor.reflection() >=  Black:
+        left_motor.run(speed*2)
+        right_motor.run(speed*2)
+        
+    while Lsensor.reflection() <=  White:
+        left_motor.run(speed*2)
+        right_motor.run(speed*-1.5)
+    
+    #while Rsensor.reflection() > =  Black:
+    #    left_motor.run(speed*0.5)
+    #   right_motor.run(speed*-1)
+    
+    for i in range(3):
+        C = 0
+        while C<10000:
+            if Lsensor.reflection() >= White:
+                left_motor.run(speed)
+            if Rsensor.reflection() >= White:
+                right_motor.run(speed)
+            C += 1
+        #####print(C)
+        C = 0
+        while C<10000:
+            if Lsensor.reflection() <= Black:
+                left_motor.run(-1*speed)
+            if Rsensor.reflection() <= Black:
+                right_motor.run(-1*speed)
+            C += 1
+            
