b81539b9bd31a07fe9dc2e37da7cdac32b0ab272
RonRed3 <ron754895@gmail.com>
2022-03-10 19:14:46 +0200

    UPDATE

--- run/two.py
+++ run/two.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env pybricks-micropython
+
 from pybricks.ev3devices import Motor, TouchSensor, ColorSensor
 from pybricks.ev3devices import InfraredSensor, UltrasonicSensor, GyroSensor
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import math
 import time
 import func.PID as PID
 import func.motor as motor
 
+PID.drive_dis(120, 42)
+PID.follow_line(70, 45)
+PID.left_turn(15)
+PID.drive_dis(120, 60)
+PID.drive_dis(-80, 18)
+PID.drive_dis(80, 15)
+motor.time_move("D", 1.3, -60)
+motor.time_move("A", 1, -200)
+PID.drive_dis(-90, 30)
+motor.time_move("D", 1, 100)
+PID.drive_dis(-120, 35)
 
-motor.time_move("A", 3, 55)
-PID.drive_dis(100, 100)
-PID.drive_dis(50, 48,-5)
-PID.drive_time(120, 1.7)
-PID.drive_dis(-60, 4)
-PID.drive_time(150, 0.7)
-PID.drive_dis(-60, 4)
-motor.time_move("D", 5, 120)
-PID.drive_time(-120, 8)
-PID.drive_dis(60, 7)
-PID.left_turn(95)
 
